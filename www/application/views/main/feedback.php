54dd2807876c6cd7d6821f628b5b29634eee2a5b
Roquie <roquie0@gmail.com>
2014-03-19 00:48:45 +0400

    кнопки назад в feedback валидации

--- www/application/views/main/feedback.php
+++ www/application/views/main/feedback.php
@@ -5,16 +5,30 @@
             <form class="form-horizontal" action="<?=Request::current()->url()?>" method="POST" accept-charset="utf-8" novalidate>
                 <fieldset>
                     <div class="row">
-                        <div class="span5 pull-left">
+                        <div class="span8 pull-left">
                             <legend>Форма обратной связи</legend>
                             <?if(isset($error)):?>
-                                <div class="alert alert-danger">
-                                    <?=$error?>
+                                <div class="row">
+                                    <div class="span1">
+                                        <a href="#" class="btn" onclick="window.history.go(-1); return false;">Назад</a>
+                                    </div>
+                                    <div class="span6">
+                                        <div class="alert alert-danger">
+                                            <?=$error?>
+                                        </div>
+                                    </div>
                                 </div>
                             <?endif?>
                             <?if(isset($success)):?>
-                                <div class="alert alert-success">
-                                    <?=$success?>
+                                <div class="row">
+                                    <div class="span1">
+                                        <a href="#" class="btn" onclick="window.history.go(-1); return false;">Назад</a>
+                                    </div>
+                                    <div class="span6">
+                                        <div class="alert alert-success">
+                                            <?=$success?>
+                                        </div>
+                                    </div>
                                 </div>
                             <?endif?>
                         </div>

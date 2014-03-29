4e5e71f9f7277b6ab014e0e902750c164f9f60bf
Roquie <roquie0@gmail.com>
2014-03-30 01:17:29 +0400

    test print 3

--- www/application/classes/Controller/Admin/Files/Print.php
+++ www/application/classes/Controller/Admin/Files/Print.php
@@ -2,10 +2,14 @@
 
 class Controller_Admin_Files_Print extends Controller_Admin_Base
 {
+    protected $_convert_url = 'http://am3-word-view.officeapps.live.com/wv/WordViewer/request.pdf?WOPIsrc=http%3A%2F%2Fam3-15-view-wopi%2Ewopi%2Elive%2Enet%3A808%2Foh%2Fwopi%2Ffiles%2F%40%2FwFileId%3FwFileId%3D';
 
-    public function action_index()
+    public function action_statement()
     {
-        $this->template->content = View::factory('admin/files/all');
+        $file = Request::factory('admin/files/download/create_statement')->execute();
+
+        HTTP::redirect($this->_convert_url.urlencode(URL::site($file)).'&type=printpdf');
+        //<?=urlencode(URL::site('/test2.docx')) &type=printpdf
     }
 
 

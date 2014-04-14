421014fd047fd1d712973624d261be1c1f13a180
roquie <roquie0@gmail.com>
2014-04-14 14:59:35 +0400

    ...

---
+++ www/application/classes/Controller/Api/V1.php
@@ -0,0 +1,17 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Controller_Api_V1 extends Controller
+{
+
+
+    public function action_test()
+    {
+        $post = $this->request->post();
+
+        $this->ajax_msg($post);
+    }
+
+
+
+
+}

2f589c62583fcd331c12c5505e545fa0067f21e8
roquie <roquie0@gmail.com>
2014-05-30 22:22:14 +0400

    по барабану как вводить тел

--- www/application/classes/Controller/Main/Index.php
+++ www/application/classes/Controller/Main/Index.php
@@ -18,6 +18,12 @@ class Controller_Main_Index extends Controller_Main_Base
             DB::update('users')->where('id', '=', $v->id)->set(array('hash' => md5(uniqid())))->execute();
         }*/
 
+   /*     $l = new Model_Listeners();
+        foreach($l->find_all() as $k => $v)
+        {
+            DB::update('listeners')->where('id', '=', $v->id)->set(array('tel' => Text::format_phone($v->tel)))->execute();
+        }*/
+
         $this->template->content = View::factory('main/index');
     }
 
--- www/application/classes/Text.php
+++ www/application/classes/Text.php
@@ -4,6 +4,21 @@ class Text extends Kohana_Text
 {
 
 
+
+    public static function format_phone($phone)
+    {
+        $number = static::removeThanDigits($phone);
+
+        return sprintf('+7 (%s) %s-%s-%s',
+                        substr($number, 1, 3),
+                        substr($number, 4, 3),
+                        substr($number, 7, 2),
+                        substr($number, 9, 2)
+        );
+    }
+
+
+
     /**
      * удаляет все, кроме цифр из строки
      * @param $str

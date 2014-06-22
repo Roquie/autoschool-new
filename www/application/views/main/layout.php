95e69633d20f8667f452b3478229388280086f8e
Roquie <roquie0@gmail.com>
2014-06-22 23:14:32 +0400

    яндекс метрика

--- www/application/views/main/layout.php
+++ www/application/views/main/layout.php
@@ -36,5 +36,8 @@
 <?=HTML::script('global/js/general_fns.js')?>
 <?=HTML::script('main/js/main.js')?>
 <?=HTML::script('global/js/global.js')?>
+
+<!-- Yandex.Metrika counter --><script type="text/javascript">(function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter25347593 = new Ya.Metrika({id:25347593, webvisor:true, clickmap:true, trackLinks:true, accurateTrackBounce:true, trackHash:true}); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks");</script><noscript><div><img src="//mc.yandex.ru/watch/25347593" style="position:absolute; left:-9999px;" alt="" /></div></noscript><!-- /Yandex.Metrika counter -->
+
 </body>
 </html>
